9137c3a8255cda3ff02bd86023065f2b75813e41
airemcs <its.airemcs@gmail.com>
2024-02-04 17:01:25 +0800

    Fix React Conventions

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,58 +2,58 @@
 
 export default function Register() {
   return (
-  <section class="bg-gray-50">
-    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+  <section className="bg-gray-50">
+    <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
 
     {/* Logo */}
-    <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900">
-    <img class="w-8 h-8 mr-2" src="src/assets/logo.png" alt="logo" />Verkosto</a>
+    <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900">
+    <img className="w-8 h-8 mr-2" src="src/assets/logo.png" alt="logo" />Verkosto</a>
 
-    <div class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
-    <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+    <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
+    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
 
-      <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">Register a New Account</h1>
+      <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">Register a New Account</h1>
 
-      <form class="space-y-4 md:space-y-6" action="#">
+      <form className="space-y-4 md:space-y-6" action="#">
 
         <div>
-          <label htmlFor="name" class="block mb-2 text-sm font-medium text-gray-900">Your Full Name</label>
-          <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Airelle Loumel Maagma" required="" />
+          <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900">Your Full Name</label>
+          <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Airelle Loumel Maagma" required="" />
         </div>
 
         <div>
-          <label htmlFor="username" class="block mb-2 text-sm font-medium text-gray-900">Your Username</label>
-          <input type="text" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="AirelleM" required="" />
+          <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900">Your Username</label>
+          <input type="text" name="username" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="AirelleM" required="" />
         </div>
 
         <div>
-          <label htmlFor="email" class="block mb-2 text-sm font-medium text-gray-900">Your Email</label>
-          <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="airelle_maagma@dlsu.edu.ph" required="" />
+          <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your Email</label>
+          <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="airelle_maagma@dlsu.edu.ph" required="" />
         </div>
 
         <div>
-          <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Password</label>
-          <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required="" />
+          <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
+          <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required="" />
         </div>
 
         <div>
-          <label for="confirm-password" class="block mb-2 text-sm font-medium text-gray-900">Confirm Password</label>
-          <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required="" />
+          <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900">Confirm Password</label>
+          <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required="" />
         </div>
 
-        <div class="flex items-start">
-          <div class="flex items-center h-5">
-          <input id="terms" aria-describedby="terms" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300" required="" />
+        <div className="flex items-start">
+          <div className="flex items-center h-5">
+          <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300" required="" />
           </div>
 
-          <div class="ml-3 text-sm">
-          <label for="terms" class="font-light text-gray-500">I accept the <a class="font-medium text-primary-600 hover:underline" href="#">Terms and Conditions</a></label>
+          <div className="ml-3 text-sm">
+          <label htmlFor="terms" className="font-light text-gray-500">I accept the <a className="font-medium text-primary-600 hover:underline" href="#">Terms and Conditions</a></label>
           </div>
         </div>
 
-        <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Create Account</button>
+        <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Create Account</button>
         
-        <p class="text-sm font-light text-gray-500">Do you already have an account? <a href="#" class="font-medium text-primary-600 hover:underline">Login Here</a>
+        <p className="text-sm font-light text-gray-500">Do you already have an account? <a href="#" className="font-medium text-primary-600 hover:underline">Login Here</a>
         </p>
 
       </form>
