245e5bfa96457549987434e621c8b57e2c412d13
Pricezy <parente.gabrielm@gmail.com>
2024-02-13 21:25:54 +0800

    Added profile header component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Login from './pages/Login.jsx'
 import NavbarRegistered from './components/NavbarRegistered.jsx'
 import Navbar from './components/Navbar.jsx'
 import Sidebar from './components/Sidebar.jsx'
+import ProfileHeader from './components/ProfileHeader.jsx'
 import Signup from './pages/Signup.jsx'
 import { Route, Routes } from 'react-router-dom'
 
@@ -9,9 +10,10 @@ export default function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Sidebar />} />
+        <Route path="/sds" element={<Sidebar />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<ProfileHeader/>} />
         <Route path="/navbar-reg" element={<NavbarRegistered />} />
       </Routes>
     </>
