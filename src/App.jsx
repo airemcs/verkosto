d8e6b91ae47b9bd17938ddccb881fb6f9e9022ee
Pricezy <parente.gabrielm@gmail.com>
2024-02-15 20:06:39 +0800

    Added Profile Page and made changed in Profile Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { Route, Routes } from 'react-router-dom'
 import Login from './pages/Login.jsx'
 import Sidebar from './components/Sidebar.jsx'
+import PostHeader from './components/ProfileHeader.jsx'
 import Signup from './pages/Signup.jsx'
+import Profile from './pages/Profile.jsx'
 
 export default function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Sidebar />} />
+        <Route path="/ds" element={<Sidebar />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<Profile />} />
       </Routes>
     </>
   )
--- src/components/ProfileHeader.jsx
+++ src/components/ProfileHeader.jsx
@@ -5,19 +5,20 @@ export default function ProfileHeader() {
   <div className="flex">
 
     <div>
-      <Link to="#"><img className="w-10 h-10 rounded-full shadow" src="src/assets/profile.jpg" /></Link>
+      <Link to="#"><img className="w-96 h-96 rounded-full shadow mx-10" src="src/assets/profile.jpg" /></Link>
     </div>
 
-    <div className="flex items-center">
-      <Link to="#"><h2 className="text-lg mx-2 font-semibold text-gray-900">Adrian Cruz</h2></Link>
-      <span className="bg-purple-100 text-purple-800 text-xs font-medium px-2.5 py-0.5 rounded border border-purple-400">President</span>
+    <div className="flex flex-col justify-end py-10">
 
-      <p className="mt-3 text-gray-700 text-sm">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris lobortis laoreet metus ac congue. Nulla at eleifend lectus. Duis tempus condimentum ligula, quis suscipit arcu sodales quis. Aenean eget lectus placerat, tincidunt dolor et, facilisis sem. Nulla ex lectus, blandit nec eros nec, feugiat rhoncus felis. Ut sit amet posuere tellus. Fusce commodo tortor finibus dolor tempus sollicitudin. Ut in maximus dui.
-     </p>
-
-     <span className="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full border border-green-400">Taguig</span>
-
+      <Link to="#"><h2 className="text-3xl mx-2  font-semibold text-gray-900 ">Adrian Cruz</h2></Link>
+      <h2 className="text-x1 mx-2  font-semibold text-gray-900 py-1 ">President</h2>
+      <h2 className="text-base mx-2  text-gray-900 py-1">Taguig</h2>
+      <h2 className="text-sm mx-2  text-gray-900 py-1">Social Medias:</h2>
+      <div className="flex">
+      <Link to="x.com"><img className="w-5 h-5 mx-3" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/X_logo_2023.svg/800px-X_logo_2023.svg.png"/></Link>
+      <Link to="facebook.com"><img className="w-5 h-5 " src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/2023_Facebook_icon.svg/800px-2023_Facebook_icon.svg.png"/></Link>
+    </div>
+    
 
     
     </div>
