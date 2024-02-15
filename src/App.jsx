8bf6ca40075bc80dc0e2749d8777152495b1c325
airemcs <its.airemcs@gmail.com>
2024-02-16 00:15:39 +0800

    Add Props

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import Login from './pages/Login.jsx'
 import Signup from './pages/Signup.jsx'
 
 import ExtendedPost from './components/post/ExtendedPost.jsx'
+import MiniPost from './components/post/MiniPost.jsx'
 
 export default function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<ExtendedPost name="ai" position="nice" title="Hello" day="Sick!" tag="nice" content="cool" />} />
+        <Route path="/" element={<MiniPost />} />
         {/* <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} /> */}
       </Routes>
--- src/components/post/MiniPost.jsx
+++ src/components/post/MiniPost.jsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
 import PostHeader from './PostHeader.jsx'
 
+// Parameters: Name, Position
+// Parameters: Title, Days, Tag, Content
 export default function MiniPost(props) {
   return (
     <div className="max-w-4xl mx-auto my-4 p-4 border border-gray-400 rounded-lg shadow-md">
@@ -14,28 +16,28 @@ export default function MiniPost(props) {
           <svg className="w-2.5 h-2.5 me-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
             <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm3.982 13.982a1 1 0 0 1-1.414 0l-3.274-3.274A1.012 1.012 0 0 1 9 10V6a1 1 0 0 1 2 0v3.586l2.982 2.982a1 1 0 0 1 0 1.414Z"/>
           </svg>
-          5 days ago
+          {props.days} days ago
         </span>
       </div>
 
       {/* title */}
       <Link to="/extended-post" className="flex justify-between items-center mb-2">
-          <h5 className="py-2 text-4xl font-semibold text-gray-900">New Fullstack Job Offer</h5>
+          <h5 className="py-2 text-4xl font-semibold text-gray-900">{props.title}</h5>
       </Link>
 
       {/* tags */}
       <div className="flex flex-wrap mb-2">
-        <span className="mr-2 mb-1 px-2 py-1 text-xs bg-green-400/30 text-slate rounded">React</span>
-        <span className="mr-2 mb-1 px-2 py-1 text-xs bg-emerald-400/30 text-slate rounded">Tailwind</span>
-        <span className="mr-2 mb-1 px-2 py-1 text-xs bg-green-400/30 text-slate rounded">Web Development</span>
+        <span className="mr-2 mb-1 px-2 py-1 text-xs bg-green-400/30 text-slate rounded">{props.tag1}</span>
+        <span className="mr-2 mb-1 px-2 py-1 text-xs bg-emerald-400/30 text-slate rounded">{props.tag2}</span>
+        <span className="mr-2 mb-1 px-2 py-1 text-xs bg-green-400/30 text-slate rounded">{props.tag3}</span>
       </div>
 
       {/* short text*/}
       <div className="mb-5">
       <p className="text-gray-600 line-clamp-1 text-base">
-        lorem ipsum awesome ipsum awesome lorem ipsum awesome ipsum awesome lorem ipsum awesome ipsum awesomelorem ipsum awe 
+          {props.content}
       </p>
-      <Link to="/extended-post" className="text-base font-semibold text-gray-500 hover:underline">Read more</Link>
+      <Link to="/extended-post" className="text-base font-semibold text-gray-500 hover:underline">Read More</Link>
       </div>
 
 
@@ -44,19 +46,19 @@ export default function MiniPost(props) {
         <button className="h-7 flex pl-1 pr-3 justify-between items-center border text-gray-600 bg-gray-200/50 border-gray-400 rounded-l-full duration-75 hover:text-emerald-500 hover:border-emerald-500 hover:border-2 hover:bg-gray-200 hover:font-medium" type="button">
           <svg className="w-6 h-4 mr-0.5" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
             <path fill="currentColor" d="M12.781 2.375c-.381-.475-1.181-.475-1.562 0l-8 10A1.001 1.001 0 0 0 4 14h4v7a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-7h4a1.001 1.001 0 0 0 .781-1.625zM15 12h-1v8h-4v-8H6.081L12 4.601L17.919 12z"/>
-          </svg><p className="text-sm">1000</p>
+          </svg><p className="text-sm">{props.likes}</p>
         </button>
         <button className="h-7 flex pl-1 pr-3 justify-between items-center bg-gray-200/50 text-gray-600 border-r border-t border-b border-gray-400 rounded-r-full mr-3 duration-75 hover:text-blue-400 hover:border-blue-300 hover:border-2 hover:bg-gray-200 hover:font-medium" type="button">
           <svg className="w-6 h-4 mr-0.5" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
             <path fill="currentColor" d="M20.901 10.566A1.001 1.001 0 0 0 20 10h-4V3a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v7H4a1.001 1.001 0 0 0-.781 1.625l8 10a1 1 0 0 0 1.562 0l8-10c.24-.301.286-.712.12-1.059M12 19.399L6.081 12H10V4h4v8h3.919z"/>
-          </svg><p className="text-sm">10</p>
+          </svg><p className="text-sm">{props.dislikes}</p>
         </button>
 
         <svg class="mr-1.5 w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 18">
           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 5h5M5 8h2m6-3h2m-5 3h6m2-7H2a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h3v5l5-5h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z"/>
         </svg>
         <Link to="extended-post/comment" className="hover:underline">
-          <p className="text-sm text-gray-600">5 Comments</p>
+          <p className="text-sm text-gray-600">{props.comments} Comments</p>
         </Link>
       </div>
     </div>
