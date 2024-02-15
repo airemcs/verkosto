4c793b8a7fdd891f834f6bd06e8f0b29c64e4162
airemcs <its.airemcs@gmail.com>
2024-02-15 11:59:49 +0800

    Adjust Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage.jsx'
 import Login from './pages/Login.jsx'
-import Sidebar from './components/Sidebar.jsx'
 import Signup from './pages/Signup.jsx'
 
 export default function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Sidebar />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+import Searchbar from '../components/Searchbar.jsx'
+import Sidebar from '../components/Sidebar.jsx'
+
+export default function HomePage() {
+  return (
+  <>
+  <Sidebar />
+  <Searchbar />
+  </>
+  )
+}
