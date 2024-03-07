f9232c51466b72f67a64f2fb860a3c337013f70e
airemcs <its.airemcs@gmail.com>
2024-03-07 08:58:19 +0800

    Connect MongoDB Database

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,22 @@
 import express from 'express';
-import { PORT } from './config.js';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`The application is listening to PORT: ${PORT}`);
+app.get('/', (req, res) => {
+  console.log(req);
+  return res.status(234).send('Hello World!');
 });
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log('The application is connected to the database.');
+    app.listen(PORT, () => {
+      console.log(`The application is listening to PORT: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
