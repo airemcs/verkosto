58d0c114da0d1bc6c5189c146a854adf6a630d00
airemcs <its.airemcs@gmail.com>
2024-03-10 18:53:07 +0800

    Add Post Tag

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,37 @@
 import express from 'express';
 import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
+import { Tag } from './models/Models.js';
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
   console.log(req);
   return res.status(234).send('Hello World!');
+});
+
+app.post('/tag', async (req, res) => {
+  try {
+    
+    if (!req.body.title || !req.body.description) {
+      return res.status(400).send({
+        message: 'Please send all the required fields: title, description.'
+      });
+    }
+
+    const newTag = {
+      title: req.body.title,
+      description: req.body.description
+    };
+
+    const tag = await Tag.create(newTag);
+    return res.status(201).send(tag);
+
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
 });
 
 mongoose
@@ -17,6 +42,6 @@ mongoose
       console.log(`The application is listening to PORT: ${PORT}`);
     });
   })
-  .catch((err) => {
-    console.log(err);
+  .catch((error) => {
+    console.log(error);
   });
