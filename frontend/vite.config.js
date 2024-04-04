2afe522b23a54d1adb48e7db8fd49d9a5d80a156
spyhard12 <rafaelsuboyap12@gmail.com>
2024-04-05 01:58:16 +0800

    trying to access public images

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,3 +5,11 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
 })
+
+module.exports = {
+  root: './',
+  build: {
+    ourDir: 'dist',
+  },
+  publicDir: 'src/assets'
+}
