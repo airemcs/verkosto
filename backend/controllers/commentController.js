dd74ee24809166e4ced4c6cdc6028655f1fd365a
Pricezy <parente.gabrielm@gmail.com>
2024-04-05 14:09:37 +0800

    added delete comment

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -55,6 +55,7 @@ const getComment = async (req, res) => {
 }
 
 const updateComment = async (req, res) => {
+  console.log("jskahdkasjhdksjah")
   const { id: commentId } = req.params;
   const { content } = req.body;
 
@@ -76,4 +77,23 @@ const updateComment = async (req, res) => {
   }
 }
 
-module.exports = { createComment, getAllComments, getComment, updateComment };
+const deleteComment = async (req, res) => {
+  console.log("anything")
+  const { id: commentId } = req.params;
+  console.log(commentId)
+  try {
+    const comment = await Comment.findById(commentId);
+    const deleteUserComment = await User.findByIdAndUpdate(comment.userID, { $pull: { commentIDs: commentId } });
+    const deletePostComment = await Post.findByIdAndUpdate(comment.postID, { $pull: { commentIDs: commentId } });
+    console.log(deletePostComment);
+    await Comment.findByIdAndDelete(commentId);
+    return res.status(200).json({ message: "Comment deleted successfully" });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+};
+
+
+
+module.exports = { createComment, getAllComments, getComment, updateComment, deleteComment };
